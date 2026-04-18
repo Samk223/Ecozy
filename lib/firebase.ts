ac49f5c1a9a6f0cf8442860a6e534178cc05e446
Somya Kashyap <145283196+Samk223@users.noreply.github.com>
2026-04-19 03:44:19 +0530

    Comment out firebaseConfigJson import
    Comment out the import of firebaseConfigJson.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import firebaseConfigJson from "../firebase-applet-config.json";
+// import firebaseConfigJson from "../firebase-applet-config.json";
 
 // Standard Firebase configuration
 const firebaseConfig = {
